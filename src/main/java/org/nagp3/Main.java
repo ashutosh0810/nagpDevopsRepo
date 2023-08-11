33c26c539b67010371024963d8fd1ae08e24f260
Project Tester <njoyashu@gmail.com>
2023-08-12 01:59:57 +0530

    test added

--- src/main/java/org/nagp3/Main.java
+++ src/main/java/org/nagp3/Main.java
@@ -6,6 +6,25 @@ import org.testng.annotations.Test;
 public class Main {
 
 
+    @org.testng.annotations.Test
+    public static void deletemethod()
+    {
+        int a = 5-5;
+        Assert.assertEquals(a,0);
+    }
+
+    @org.testng.annotations.Test
+    public static void multiplyumethod()
+    {
+        int a = 5*5;
+        Assert.assertEquals(a,25);
+    }
+    @org.testng.annotations.Test
+    public static void multiply3umethod()
+    {
+        int a = 15*5;
+        Assert.assertEquals(a,75);
+    }
 
 
 }
