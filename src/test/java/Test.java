aaee1f79dc8d621e6e6c6e2879ab16037aae95e5
Shashank <shashank.cse91@gmail.com>
2023-08-12 23:05:30 +0530

    Push Sonar

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -12,21 +12,21 @@ public class Test {
     @org.testng.annotations.Test
     public static void deletemethod()
     {
-        int a = 5-5;
-        Assert.assertEquals(a,0);
+        int a = 10-5;
+        Assert.assertEquals(a,5);
     }
 
-//    @org.testng.annotations.Test
-//    public static void multiplyumethod()
-//    {
-//        int a = 5*5;
-//        Assert.assertEquals(a,25);
-//    }
-//    @org.testng.annotations.Test
-//    public static void multiply3umethod()
-//    {
-//        int a = 15*5;
-//        Assert.assertEquals(a,75);
-//    }
+    @org.testng.annotations.Test
+    public static void multiplyumethod()
+    {
+        int a = 5*5;
+        Assert.assertEquals(a,25);
+    }
+    @org.testng.annotations.Test
+    public static void multiply3umethod()
+    {
+        int a = 15*5;
+        Assert.assertEquals(a,75);
+    }
 
 }
