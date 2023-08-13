5c84190d046ba85b6d30e3affe596ca5b909d8bd
Ashutosh Kumar Singh <>
2023-08-13 18:51:50 +0530

    testcal

--- src/test/java/TestCal.java
+++ src/test/java/TestCal.java
@@ -1,10 +1,8 @@
 import org.nagp3.CalculatorApp;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import static org.testng.Assert.assertEquals;
 
 public class TestCal {
-
     private CalculatorApp calculator;
 
     @BeforeClass
@@ -14,22 +12,22 @@ public class TestCal {
 
     @org.testng.annotations.Test
     public void testAdd() {
-
-        Assert.assertEquals(calculator.add(2,4),6);
-        Assert.assertEquals(calculator.add(2,5),7);
+        Assert.assertEquals(calculator.add(2, 4), 6);
+        Assert.assertEquals(calculator.add(2, 5), 7);
     }
 
     @org.testng.annotations.Test
     public void testSubtract() {
-        Assert.assertEquals(calculator.subtract(6,5),1);
-            }
+        Assert.assertEquals(calculator.subtract(6, 5), 1);
+    }
 
     @org.testng.annotations.Test
     public void testmutiply() {
-        Assert.assertEquals(calculator.multiply(2,5),10);
+        Assert.assertEquals(calculator.multiply(2, 5), 10);
     }
+
     @org.testng.annotations.Test
     public void testdivide() {
-        Assert.assertEquals(calculator.divide(2,2),1);
+        Assert.assertEquals(calculator.divide(2, 2), 1);
     }
 }
